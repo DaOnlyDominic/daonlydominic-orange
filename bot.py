f184934c32f64cdecf98a1be768ff9fc6845991d
DaOnlyDominic <txmotefam@gmail.com>
2019-01-16 10:34:00 -0600

    Update bot.py

--- bot.py
+++ bot.py
@@ -9,6 +9,13 @@ async def on_ready():
     print(client.user.id)
     print('------')
     await client.change_presence(game=discord.Game(name="--Insert-Game-name-here--"))
+    
+@client.event
+async def on_message(message):
+    if message.author != client.user:
+      if message.content.startswith('bot.hello'):
+        msg = 'Hello {0.author.mention}'.format(message)
+        await client.send_message(message.channel, msg)
 
 token = os.environ.get("DISCORD_BOT_SECRET")
 client.run(token)
