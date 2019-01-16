71002d2556c7b67fbabfc515c1bd6ad6ac3af3db
DaOnlyDominic <txmotefam@gmail.com>
2019-01-16 10:44:50 -0600

    Update bot.py

--- bot.py
+++ bot.py
@@ -8,7 +8,7 @@ async def on_ready():
     print(client.user.name)
     print(client.user.id)
     print('------')
-    await client.change_presence(game=discord.Game(name="DaOnlyDominic Stuff"))
+    await client.change_presence(game=discord.Game(name="Python 3.6.7"))
     
 @client.event
 async def on_message(message):
