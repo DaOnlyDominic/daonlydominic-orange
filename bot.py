c49b4282c35c9924d92340cd5921b1a3b6dab5aa
DaOnlyDominic <txmotefam@gmail.com>
2019-01-16 10:32:33 -0600

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,1 +1,14 @@
+import os, discord
 
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    print('Logged in as')
+    print(client.user.name)
+    print(client.user.id)
+    print('------')
+    await client.change_presence(game=discord.Game(name="--Insert-Game-name-here--"))
+
+token = os.environ.get("DISCORD_BOT_SECRET")
+client.run(token)
